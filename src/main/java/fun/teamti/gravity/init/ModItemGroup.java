c525da45aed53ac69138f634618a753a93cce66f
KhanhTIMN <khanhlcbb@gmail.com>
2024-09-12 03:41:39 +0700

    Added potions to creative tab

--- src/main/java/fun/teamti/gravity/init/ModItemGroup.java
+++ src/main/java/fun/teamti/gravity/init/ModItemGroup.java
@@ -5,6 +5,8 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -22,11 +24,22 @@ public class ModItemGroup {
                     .displayItems((config, output) -> {
                         output.accept(ModBlock.PLATING_BLOCK.get());
 
-                        List<ItemStack> stacks = ModItem.ITEMS.getEntries()
+                        List<ItemStack> gravityItems = ModItem.ITEMS.getEntries()
                                 .stream()
                                 .filter(registryObject -> registryObject.get() != ModItem.CREATIVE_TAB_ICON.get().asItem())
-                                .map(reg -> new ItemStack(reg.get())).toList();
-                        output.acceptAll(stacks);
+                                .map(reg -> new ItemStack(reg.get()))
+                                .toList();
+
+                        output.acceptAll(gravityItems);
+
+                        List<ItemStack> gravityPotions = ModPotion.POTIONS.getEntries()
+                                .stream()
+                                .map(reg -> PotionUtils.setPotion(new ItemStack(Items.POTION), reg.get()))
+                                .toList();
+
+                        output.acceptAll(gravityPotions);
+
+
                     })
                     .build());
 }
